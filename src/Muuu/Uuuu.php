362399c4cecd88db50e9edf153708cc158ffd4a8
Hpscript <narishige7@gmail.com>
2023-01-20 13:18:21 +0000

    6th

--- src/Muuu/Uuuu.php
+++ src/Muuu/Uuuu.php
@@ -1,14 +1,7 @@
 <?php
 
-class Dog {
+namespace shibuya;
 
-	private $name;
-
-	public function __construct($name){
-		$this->name = $name;
-	} 
-
-	public function getName(){
-		return $this->name . "さん!";
-	}
+function getName(){
+	return "shibuya";
 }
